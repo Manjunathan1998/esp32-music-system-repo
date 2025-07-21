1a39eeccc4a9956e5b2c641aec3c3f88a869916a
jsonslim <deluxe87d@gmail.com>
2025-07-21 18:34:39 +0200

    init

---
+++ src/defines.h
@@ -0,0 +1,26 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define I2S_BCK 27
+#define I2S_WS 25
+#define I2S_DATA 26
+
+#define ENCODER_CLK_PIN 33
+#define ENCODER_DT_PIN 4
+#define ENCODER_BTN_PIN 32
+#define ENCODER_BTN_HOLD_TIME 2000
+
+#define LGFX_USE_V1 // needed only for lovyanGfx lib
+
+#define TFT_W 320
+#define TFT_H 480
+
+#define TFT_BL_PIN 23 // display backlight pin
+#define TFT_CS_PIN 15
+#define TFT_RST_PIN 22
+#define TFT_DC_PIN 21
+#define TFT_SCLK_PIN 14
+#define TFT_MOSI_PIN 13
+#define TFT_MISO_PIN -1
+
+#endif
